1ff619316f4208c4568faff48124d0dedb7a8098
Bipul20000 <bipul.dtu@gmail.com>
2026-01-18 19:08:29 +0530

    frontend done

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -18,7 +18,7 @@ const LatestCollection = () => {
     <div className='my-10'>
       <div className='text-center py-8 text-3xl'>
         < Title text1={'LATEST'} text2 = {'COLLECTION'}/>
-        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>shila shila ki jawani, i am too sexy for you, me tere hath na ani</p>
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Namaste Uncle</p>
       </div>
         {/* rendering products */}
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
